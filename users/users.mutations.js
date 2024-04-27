5c3e0f1ddae8baba270df1e487957adba968f8dd
minsu.lee <minsu.rob@gmail.com>
2024-04-27 20:30:18 +0900

    4.4유저 검색 및 로그인 검증 실패까지 추가

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -39,6 +39,23 @@ export default {
             } catch (error) {
                 return error;
             }
-        }
+        },
+        login: async (_, {username, password}) => {
+            const user = await client.user.findFirst( { where : {username} });
+            if (!user) {
+                return {
+                    ok: false,
+                    error: "user not found"
+                }
+            }
+
+            const passwordOk = await bcrypt.compare(password, user.password);
+            if (!passwordOk) {
+                return {
+                    ok: false,
+                    error: "Incorrect pwd."
+                }
+            }
+        },
     }
 }
--- users/users.typeDefs.js
+++ users/users.typeDefs.js
@@ -10,6 +10,13 @@ export default gql`
     createdAt: String!
     updatedAt: String!
   }
+
+  type LoginResult {
+    ok: Boolean!
+    token: String
+    error: String
+  }
+
   type Mutation {
     createAccount(
       firstName: String!
@@ -18,6 +25,8 @@ export default gql`
       email: String!
       password: String!
     ): User
+    
+    login(username: String!, password: String!): LoginResult!
   }
   type Query {
     seeProfile(username: String!): User
