0ff015a2f732edd3bfe7048e60933f580dffccb3
minsu.lee <minsu.rob@gmail.com>
2024-04-27 22:37:00 +0900

    4.7 - 프로필 update 기본 처리 추가

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,6 +1,22 @@
+import client from '../../client';
 export default {
     Mutation: {
-        editProfile: (_, { firstName, lastName, username, email, password }) =>
-            console.log('hi'),
+        editProfile: (
+            _,
+            { firstName, lastName, username, email, password },
+        ) => {
+            return client.user.update({
+                where: {
+                    id: 2,
+                },
+                data: {
+                    firstName,
+                    lastName,
+                    username,
+                    email,
+                    password,
+                },
+            });
+        },
     },
 };
