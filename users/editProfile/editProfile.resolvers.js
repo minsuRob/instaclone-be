99498692799daea7970d5cb2c470e1f325cf7d91
minsu.lee <minsu.rob@gmail.com>
2024-04-30 17:15:00 +0900

    4.8 - 추상화 안된 jwt 검증 처리, 중복코드 제거 필요

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,18 +1,28 @@
+import jwt from 'jsonwebtoken';
 import client from '../../client';
 import bcrypt from 'bcrypt';
 export default {
     Mutation: {
         editProfile: async (
             _,
-            { firstName, lastName, username, email, password: newPassword },
+            {
+                firstName,
+                lastName,
+                username,
+                email,
+                password: newPassword,
+                token,
+            },
         ) => {
+            const { id } = await jwt.verify(token, process.env.SECRET_KEY);
+
             let uglyPwd = null;
             if (newPassword) {
                 uglyPwd = await bcrypt.hash(newPassword, 10);
             }
             const updateUser = await client.user.update({
                 where: {
-                    id: 2,
+                    id,
                 },
                 data: {
                     firstName,
