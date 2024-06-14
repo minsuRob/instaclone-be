938d6718ed113521c6e04625494a73853cdc071e
minsu.lee <minsu.rob@gmail.com>
2024-06-15 05:30:12 +0900

    4.17 - 업로드 정의 추가 및 버퍼 오류 수정

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 import { ApolloServer } from 'apollo-server';
-import { resolvers, typeDefs } from './schema';
+import { typeDefs, resolvers } from './schema';
 import { getUser, protectResolver } from './users/users.utils';
 
 const PORT = process.env.PORT;
