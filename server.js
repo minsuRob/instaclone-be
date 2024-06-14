55c30b94e304a022099eb28b72c09b8ddea35544
minsu.lee <minsu.rob@gmail.com>
2024-06-15 07:04:30 +0900

    nico와 4.17버전에 맞추어 package 업데이트

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
-import { ApolloServer } from 'apollo-server';
-import { resolvers, typeDefs } from './schema';
-import { getUser, protectResolver } from './users/users.utils';
+import express from 'express';
+import logger from 'morgan';
+import { ApolloServer } from 'apollo-server-express';
+import { typeDefs, resolvers } from './schema';
+import { getUser } from './users/users.utils';
 
 const PORT = process.env.PORT;
 const server = new ApolloServer({
@@ -10,12 +12,13 @@ const server = new ApolloServer({
     context: async ({ req }) => {
         return {
             loggedInUser: await getUser(req.headers.token),
-            protectResolver,
         };
     },
 });
-server
-    .listen(PORT)
-    .then(() =>
-        console.log(`🚀Server is running on http://localhost:${PORT} ✅`),
-    );
+
+const app = express();
+app.use(logger('tiny'));
+server.applyMiddleware({ app });
+app.listen({ port: PORT }, () => {
+    console.log(`🚀Server is running on http://localhost:${PORT} ✅`);
+});
