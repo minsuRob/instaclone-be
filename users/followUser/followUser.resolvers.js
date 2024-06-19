9a90443592cbe2a9f0f9a2eea660a5a07d41913a
minsu.lee <minsu.rob@gmail.com>
2024-06-19 12:50:45 +0900

    4.21: graphql로 follow update 할수있게 변경

---
+++ users/followUser/followUser.resolvers.js
@@ -0,0 +1,22 @@
+import client from '../../client';
+import { protectResolver } from '../users.utils';
+
+export default {
+    Mutation: {
+        followUser: protectResolver(
+            async (_, { toFollow }, { loggedInUser }) =>
+                await client.user.update({
+                    where: {
+                        id: loggedInUser.id,
+                    },
+                    data: {
+                        following: {
+                            connect: {
+                                username: toFollow,
+                            },
+                        },
+                    },
+                }),
+        ),
+    },
+};
