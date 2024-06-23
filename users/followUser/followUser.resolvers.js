3dabfcdbb700168aaa5d8a4c397499b6e9060570
minsu.lee <minsu.rob@gmail.com>
2024-06-23 15:30:06 +0900

    4.21: 팔로우 유저 유효성 체크

--- users/followUser/followUser.resolvers.js
+++ users/followUser/followUser.resolvers.js
@@ -5,6 +5,19 @@ export default {
     Mutation: {
         followUser: protectResolver(
             async (_, { username }, { loggedInUser }) => {
+                const ok = await client.user.findUnique({
+                    where: {
+                        username,
+                    },
+                });
+
+                if (!ok) {
+                    return {
+                        ok: false,
+                        error: 'That user does not exist.',
+                    };
+                }
+
                 await client.user.update({
                     where: {
                         id: loggedInUser.id,
