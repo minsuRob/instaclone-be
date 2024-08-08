fc31e91c250280ba10dcb6d634e7f6865ada2237
minsu.lee <minsu.rob@gmail.com>
2024-08-09 00:01:37 +0900

    6.3: 캡션에 존재하는 값을 db에 저장
    존재여부를 통하여 데이터 저장

--- photos/uploadPhoto/uploadPhoto.resolvers.js
+++ photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -1,12 +1,27 @@
-import { protectResolver } from '../users/users.utils';
+import client from '../../client';
+import { protectResolver } from '../../users/users.utils';
 
 export default {
     Mutation: {
         uploadPhoto: protectResolver(
             async (_, { file, caption }, { loggedInUser }) => {
                 if (caption) {
-                    //
+                    const hashtags = caption.match(/#[\w]+/g);
                 }
+                client.photo.create({
+                    data: {
+                        file,
+                        caption,
+                        hashtags: {
+                            connectOrCreate: [
+                                {
+                                    where: { hashtag: '#food' },
+                                    create: { hashtag: '#food' },
+                                },
+                            ],
+                        },
+                    },
+                });
             },
         ),
     },
