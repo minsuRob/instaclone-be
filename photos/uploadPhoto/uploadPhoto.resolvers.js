47e9590bfbcaeff34f133d691ad0a57e092040f4
minsu.lee <minsu.rob@gmail.com>
2024-08-09 00:09:03 +0900

    6.4: hash obj 선작업
    user unique 처리는 확인 필요

--- photos/uploadPhoto/uploadPhoto.resolvers.js
+++ photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -5,21 +5,40 @@ export default {
     Mutation: {
         uploadPhoto: protectResolver(
             async (_, { file, caption }, { loggedInUser }) => {
+                let hashtagObj = [];
                 if (caption) {
                     const hashtags = caption.match(/#[\w]+/g);
+                    hashtagObj = hashtags.map(() => ({
+                        where: { hashtag },
+                        create: { hashtag },
+                    }));
                 }
                 client.photo.create({
                     data: {
                         file,
                         caption,
-                        hashtags: {
+                        /*hashtags: {
                             connectOrCreate: [
                                 {
                                     where: { hashtag: '#food' },
                                     create: { hashtag: '#food' },
                                 },
                             ],
+                        },*/
+
+                        user: {
+                            connect: {
+                                id: loggedInUser.id,
+                            },
                         },
+
+                        ...(
+                            hashtagObj.length > 0 && {
+                                hashtags: {
+                                    connectOrCreate: hashtagObj,
+                                },
+                            },
+                        ),
                     },
                 });
             },
