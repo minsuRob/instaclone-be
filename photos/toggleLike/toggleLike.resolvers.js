8be9ef3b59ce080cd21b288cfccd86cf559d1d41
minsu.lee <minsu.rob@gmail.com>
2024-08-09 20:53:43 +0900

    6.10: toggle like 처리 추가

--- photos/toggleLike/toggleLike.resolvers.js
+++ photos/toggleLike/toggleLike.resolvers.js
@@ -4,16 +4,51 @@ import { protectResolver } from '../../users/users.utils';
 export default {
     Mutation: {
         toggleLike: protectResolver(async (_, { id }, { loggedInUser }) => {
-            const ok = await client.photo.findUnique({
+            const photo = await client.photo.findUnique({
                 where: {
                     id,
                 },
             });
 
-            if (!ok) {
+            if (!photo) {
                 return {
                     ok: false,
                     error: 'Photo not found',
+                };
+            }
+
+            const likeWhere = {
+                photoId_userId: {
+                    userId: loggedInUser.id,
+                    photoId: id,
+                },
+            };
+
+            const like = await client.like.findUnique({
+                where: likeWhere,
+            });
+
+            if (like) {
+                await client.like.delete({
+                    where: likeWhere,
+                });
+            } else {
+                await client.like.create({
+                    data: {
+                        user: {
+                            connect: {
+                                id: loggedInUser.id,
+                            },
+                        },
+                    },
+                    photo: {
+                        connect: {
+                            id: photo.id,
+                        },
+                    },
+                });
+                return {
+                    ok: true,
                 };
             }
         }),
