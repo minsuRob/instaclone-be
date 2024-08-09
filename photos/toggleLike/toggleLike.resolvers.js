073522612fdb153acd4521836eeea0b3179608a2
minsu.lee <minsu.rob@gmail.com>
2024-08-09 20:57:29 +0900

    fix: change syntax

--- photos/toggleLike/toggleLike.resolvers.js
+++ photos/toggleLike/toggleLike.resolvers.js
@@ -9,25 +9,21 @@ export default {
                     id,
                 },
             });
-
             if (!photo) {
                 return {
                     ok: false,
                     error: 'Photo not found',
                 };
             }
-
             const likeWhere = {
                 photoId_userId: {
                     userId: loggedInUser.id,
                     photoId: id,
                 },
             };
-
             const like = await client.like.findUnique({
                 where: likeWhere,
             });
-
             if (like) {
                 await client.like.delete({
                     where: likeWhere,
@@ -40,17 +36,18 @@ export default {
                                 id: loggedInUser.id,
                             },
                         },
-                    },
-                    photo: {
-                        connect: {
-                            id: photo.id,
+                        photo: {
+                            connect: {
+                                id: photo.id,
+                            },
                         },
                     },
                 });
-                return {
-                    ok: true,
-                };
             }
+
+            return {
+                ok: true,
+            };
         }),
     },
 };
