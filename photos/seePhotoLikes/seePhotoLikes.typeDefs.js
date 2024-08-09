a75ce913b5babb0146841808c742d7388d5c0c84
minsu.lee <minsu.rob@gmail.com>
2024-08-09 20:19:03 +0900

    6.10: photo의 user 좋아요만큼 수를 알수있는 함수

--- photos/seePhotoLikes/seePhotoLikes.typeDefs.js
+++ photos/seePhotoLikes/seePhotoLikes.typeDefs.js
@@ -1,11 +1,7 @@
 import { gql } from 'apollo-server';
 
 export default gql`
-    type LikePhotoResult {
-        ok: Boolean!
-        error: String
-    }
-    type Mutation {
-        likePhoto(id: Int!): LikePhotoResult
+    type Query {
+        seePhotoLikes(id: Int!): [User]
     }
 `;
